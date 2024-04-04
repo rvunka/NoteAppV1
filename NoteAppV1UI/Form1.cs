a7bd2450b85b5b4b6c72bf5db929acf990576d41
rvunka <grach03@list.ru>
2024-04-04 15:27:02 +0300

    Добавлен проект логики

--- NoteAppV1UI/Form1.cs
+++ NoteAppV1UI/Form1.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Drawing; 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NoteAppV1;
 
 namespace NoteAppV1UI
 {
